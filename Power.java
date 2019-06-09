e4fbcdb4825ddf16cb84f9cd84d7f4628cf8ecc5
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-09 15:58:08 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+ public class Power{
+  public static void main(String[] args) {
+    double l,m;
+  Scanner sc=new Scanner(System.in);
+  l=sc.nextInt();
+  m=sc.nextInt();
+    
+   int result = (int)Math.pow(l, m);
+   System.out.println(result);
+
+  }
+}
