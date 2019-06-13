3664f2a84dff85b6b591af5b86c52bb8012d437e
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-13 15:50:18 +0530

    Create Sum6.java

---
+++ Sum6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+public class Natural 
+{
+    public static void main(String args[])
+    {
+        int p, i = 1 ;
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        p= sc.nextInt();
+        while(i <= p)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println(sum);
+    } 
+}
