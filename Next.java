beb3a0dd0b2b4495573587066a2a0dc5a0149187
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-13 16:18:14 +0530

    Create Next.java

---
+++ Next.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Next
+{
+	public static void main (String[] args) 
+	{
+		int n,i;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		i=n+1;
+		System.out.println(i);	
+	}
+}
