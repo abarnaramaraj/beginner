bad7c4538932c5e32de09ea7c36fd5c065dfc71e
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-13 16:13:58 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Add
+{
+	public static void main (String[] args)
+	{
+		int n,m,i;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		m=sc.nextInt();
+		i=n+m;
+		System.out.println(i);	
+	}
+}
