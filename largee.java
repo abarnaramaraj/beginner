5cba5fd88df156f0762d0333c7e14f7ae4c319a9
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-18 19:35:09 +0530

    Update and rename Large.c to largee.java

---
+++ largee.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Largee
+{
+	public static void main (String[] args) 
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		if(a>=b&&a>=c)
+		System.out.println(a);
+		if(b>=a&&b>=c)
+		System.out.println(b);
+		if(c>=a&&c>=b)
+		System.out.println(c);
+	}
+}
