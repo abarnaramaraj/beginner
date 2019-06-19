811378f7ae23724bf976798d32d9662f2023c33d
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 11:50:40 +0530

    Create Evenn.java

---
+++ Evenn.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Evenn
+{
+	public static void main (String[] args) 
+	{
+		int a,b,i;
+       Scanner sc=new Scanner(System.in);
+       a=sc.nextInt();
+       b=sc.nextInt();
+  for (i=a+1;i<b;i++)
+{
+   if(i%2==0)
+{
+System.out.print(i);
+}
+	}
+}
+}
