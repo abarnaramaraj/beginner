dda44d89cd5d0178e64ad8922290c5a00f35c989
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 11:29:14 +0530

    Create Oddn.java

---
+++ Oddn.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Oddn
+{
+	public static void main (String[] args) 
+	{
+		int a,b,i;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+for (i=a+1;i<b;i++)
+{
+
+if(i%2!=0)
+{
+System.out.println(i);
+}
+	}
+}
+}
