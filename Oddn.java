87e275f16598311b576f5408be618d944ec049a2
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 11:47:09 +0530

    Update Oddn.java

--- Oddn.java
+++ Oddn.java
@@ -6,15 +6,15 @@ public class Oddn
 	public static void main (String[] args) 
 	{
 		int a,b,i;
-Scanner sc=new Scanner(System.in);
-a=sc.nextInt();
-b=sc.nextInt();
+          Scanner sc=new Scanner(System.in);
+          a=sc.nextInt();
+          b=sc.nextInt();
 for (i=a+1;i<b;i++)
 {
 
 if(i%2!=0)
 {
-System.out.println(i);
+System.out.print(i);
 }
 	}
 }
